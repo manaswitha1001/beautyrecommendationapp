d854d32a71f75fdf4f598f264c5e15796649a592
Lakshmimanaswitha Chimakurthi <chimakurthi.l@husky.neu.edu>
2023-11-27 00:32:42 -0800

    updated and refactored code

--- app.py
+++ app.py
@@ -9,42 +9,137 @@ from scipy.sparse import hstack
 from surprise import Dataset, Reader, KNNBasic, SVD, NMF, dump
 from surprise.model_selection import train_test_split
 
-#Load product data
-df_product_info = pd.read_csv("https://raw.githubusercontent.com/manaswitha1001/beautyrecommendationapp/main/datasets/product_info.csv", header = 0)
-df_product = df_product_info[['product_id', 'product_name', 'brand_name','ingredients','highlights', 'primary_category', 'secondary_category', 'tertiary_category']]
+# Load product data
+
+df_product_info = pd.read_csv("https://raw.githubusercontent.com/manaswitha1001/beautyrecommendationapp/main/datasets/product_info.csv", header=0)
+
+# Select relevant columns from the product data
+df_product = df_product_info[['product_id', 'product_name', 'brand_name', 'ingredients', 'highlights', 'primary_category', 'secondary_category', 'tertiary_category']]
+
+# Fill missing values in 'tertiary_category' with values from 'secondary_category'
 df_product['tertiary_category'].fillna(df_product['secondary_category'], inplace=True)
+
+# Drop rows with missing values in any column
 df_product = df_product.dropna()
 
-#Load reviews data
-df_reviews = pd.read_csv('https://raw.githubusercontent.com/manaswitha1001/beautyrecommendationapp/main/datasets/reviews.csv', header = 0, low_memory=False)
+# Load reviews data
+df_reviews = pd.read_csv('https://raw.githubusercontent.com/manaswitha1001/beautyrecommendationapp/main/datasets/reviews.csv', header=0, low_memory=False)
+
+# Select relevant columns from the reviews data
 df_review = df_reviews[['author_id', 'product_id', 'rating']]
+
+# Remove duplicate rows based on 'author_id' and 'product_id'
 df_review = df_review.drop_duplicates()
+
+# Create a reader object for collaborative filtering
 reader = Reader(rating_scale=(0, 5))
+
+# Load review data into a Surprise Dataset
 review_data = Dataset.load_from_df(df_review, reader)
+
+# Split the dataset into training and testing sets
 trainset, testset = train_test_split(review_data, test_size=.2, random_state=42)
+
+# Build a full training set for collaborative filtering
 trainset = review_data.build_full_trainset()
 
-#utility functions 
+
+# Utility Functions 
+def get_recommendations(product_id, top_n):
+    """
+    Get product recommendations based on similarity.
+
+    Parameters:
+    - product_id (str): The ID of the product for which recommendations are sought.
+    - top_n (int): The number of top recommendations to retrieve.
+
+    Returns:
+    pandas.DataFrame: A DataFrame containing the top recommendations with additional information.
+    """
+    # Find the index of the given product_id in the DataFrame
+    product_index = df_product[df_product['product_id'] == product_id].index[0]
+
+    # Retrieve the similarity scores for the given product
+    similarities = similarity_matrix[product_index]
+
+    # Find the indices of the most similar products (excluding the product itself)
+    similar_indices = np.argsort(similarities)[::-1][1:top_n+1]
+
+    # Extract relevant information for the recommended products
+    recommendations = df_product.loc[similar_indices, ['product_id', 'product_name', 'brand_name', 'tertiary_category']]
+
+    # Renmae the columns
+    column_mapping = {
+        'product_id': 'ID',
+        'product_name': 'Product Name',
+        'brand_name': 'Brand',
+        'tertiary_category': 'Category'
+    }
+    recommendations = recommendations.rename(columns=column_mapping)
+    
+    # Add a 'rank' column to the recommendations
+    recommendations.insert(0, 'Rank', range(1, top_n+1))
+
+    # Return the recommendations DataFrame
+    return recommendations
+
 
 def get_rated_products_by_user(user_id, df_review, df_product_info):
-    # Filter reviews by the user and high ratings
+    """
+    Get the list of products that a user has interacted with and rated highly.
+
+    Parameters:
+    - user_id (int): The ID of the user for whom rated products are being retrieved.
+    - df_review (pandas.DataFrame): The DataFrame containing review information.
+    - df_product_info (pandas.DataFrame): The DataFrame containing product information.
+
+    Returns:
+    pandas.DataFrame: A DataFrame containing product details with renamed columns.
+    """
+    # Filter reviews for the given user with ratings >= 4.0
     user_ratings = df_review[(df_review['author_id'] == user_id) & (df_review['rating'] >= 4.0)]
 
-    # Merge with product information
+    # Merge with product information using the 'product_id' column
     merged_data = pd.merge(user_ratings, df_product_info, on='product_id')
 
+    # Display a message indicating products with high ratings by the user
     print("\nProducts with High Ratings by User", user_id, ":")
-    
-    # Extract relevant columns
-    product_details = merged_data[['product_id', 'product_name', 'primary_category', 'rating_x']]
 
-    return product_details.reset_index(drop=True)
+    # Extract relevant columns from the merged data
+    product_details = merged_data[['product_id', 'product_name', 'brand_name', 'primary_category', 'rating_x']]
+
+    # Reset the index of the resulting DataFrame
+    product_details = product_details.reset_index(drop=True)
+
+    # Rename the columns
+    column_mapping = {
+        'product_id': 'ID',
+        'product_name': 'Product Name',
+        'brand_name': 'Brand',
+        'primary_category': 'Category',
+        'rating_x': 'User_Rating'
+    }
+    product_details = product_details.rename(columns=column_mapping)
+
+    # Return the DataFrame with renamed columns
+    return product_details
 
 
 
-# The method get_user_recommendations(user_id, model, df_review, df_product_info) takes the input user_id, model type review info and produict info and returns the list of products which can be a potential recommendations for the user. 
+def get_user_recommendations(user_id, model, df_review, df_product_info, n):
+    """
+    Get a list of potential product recommendations for a user based on collaborative filtering.
 
-def get_user_recommendations(user_id, model, df_review, df_product_info, n=10):
+    Parameters:
+    - user_id (int): The ID of the user for whom recommendations are being generated.
+    - model: The collaborative filtering model used for predictions.
+    - df_review (pandas.DataFrame): The DataFrame containing review information.
+    - df_product_info (pandas.DataFrame): The DataFrame containing product information.
+    - n (int, optional): The number of top recommendations to retrieve. Default is 5.
+
+    Returns:
+    pandas.DataFrame: A DataFrame containing top-N product recommendations with details.
+    """
     # Get a list of all unique product IDs
     all_product_ids = df_review['product_id'].unique()
 
@@ -67,52 +162,46 @@ def get_user_recommendations(user_id, model, df_review, df_product_info, n=10):
         # Find the product details in df_product_info
         product_details = df_product_info[df_product_info['product_id'] == product_id]
 
-        if len(product_details) > 0:
+        if not product_details.empty:
             product_name = product_details['product_name'].values[0]
+            brand_name = product_details['brand_name'].values[0]
             primary_category = product_details['primary_category'].values[0]
 
             # Append the recommendation to the list
-            recommendations.append([i, product_id, product_name, primary_category])
+            recommendations.append([i, product_id, product_name, brand_name,primary_category])
 
-    columns = ['Rank', 'Product_ID', 'Product_Name', 'Primary_Category']
+    columns = ['Rank', 'Product_ID', 'Product Name', 'Brand', 'Category']
     recommendations_df = pd.DataFrame(recommendations, columns=columns)
 
     return recommendations_df
 
-def get_recommendations(product_id, top_n):
-    product_index = df_product[df_product['product_id'] == product_id].index[0]
-    similarities = similarity_matrix[product_index]
-    similar_indices = np.argsort(similarities)[::-1][1:top_n+1]  # Exclude the product itself
-    recommendations = df_product.loc[similar_indices, ['product_id','product_name', 'brand_name', 'primary_category', 'tertiary_category']]
-    recommendations_reset = recommendations.reset_index(drop=True)
-    recommendations_reset.insert(0, 'rank', range(1, top_n+1))  # Add a rank column at the beginning
-    recommendations_reset = recommendations_reset.iloc[:, 1:]
-    return recommendations_reset
 
 #TFIDF model 
-tfidf = TfidfVectorizer()
-ingredient_vector = tfidf.fit_transform(df_product['ingredients'])
-highlights_vector = tfidf.fit_transform(df_product['highlights'])
-prim_category_vector = tfidf.fit_transform(df_product['primary_category'])
-sec_category_vector = tfidf.fit_transform(df_product['secondary_category'])
-tert_category_vector = tfidf.fit_transform(df_product['tertiary_category'])
-
-
-# Combine ingredient , highlights and category vectors
-feature_vectors = hstack((ingredient_vector,highlights_vector, prim_category_vector,sec_category_vector, tert_category_vector))
-similarity_matrix = cosine_similarity(feature_vectors)
+@st.cache_data
+def compute_similarity_matrix():
+    tfidf = TfidfVectorizer()
+    ingredient_vector = tfidf.fit_transform(df_product['ingredients'])
+    highlights_vector = tfidf.fit_transform(df_product['highlights'])
+    prim_category_vector = tfidf.fit_transform(df_product['primary_category'])
+    sec_category_vector = tfidf.fit_transform(df_product['secondary_category'])
+    tert_category_vector = tfidf.fit_transform(df_product['tertiary_category'])
+    # Combine ingredient , highlights and category vectors
+    feature_vectors = hstack((ingredient_vector,highlights_vector, prim_category_vector,sec_category_vector, tert_category_vector))
+    similarity_matrix = cosine_similarity(feature_vectors)
+    return similarity_matrix
 df_product = df_product.reset_index(drop=True)
 
 
 ##SVD MODEL
+
 best_params = {'n_factors': 100, 'n_epochs': 50, 'lr_all': 0.01, 'reg_all': 0.1}
 svd_best_model = SVD(**best_params)
 svd_best_model.fit(review_data.build_full_trainset())
 
 #KNN MODEL
-# best_params = {'k':10}
-# knn_best_model = KNNBasic(**best_params)
-# knn_best_model.fit(review_data.build_full_trainset())
+best_params = {'k':10}
+knn_best_model = KNNBasic(**best_params)
+knn_best_model.fit(review_data.build_full_trainset())
 
 
 # Add background image 
@@ -123,6 +212,7 @@ def add_bg_from_github(image_url):
         .stApp {{
             background-image: url({image_url});
             background-size: cover;
+            
         }}
         </style>
         """,
@@ -135,30 +225,30 @@ add_bg_from_github(background_image_url)
 
 
 # Side bar 
-st.sidebar.header('User Input')
 preference = st.sidebar.slider('Choose number of recommendations', 0, 10, 5)
-page_options = ['Trendspotter','TraitPicker','SquadSuggest']
+page_options = ['FavFinder','TraitPicker','SquadSuggester']
 choice = st.sidebar.selectbox('Select Page', page_options)
 
 
-if choice == 'Trendspotter':
-    st.write('Trending Now')
-    df_product_info['product_brand'] = df_product_info['brand_name'] + ' - ' + df_product_info['product_name']
+if choice == 'FavFinder':
+    st.title('Most loved Products')
     df_sorted = df_product_info.sort_values(by='loves_count', ascending=False)
-    top_products = df_sorted['product_brand'].head(n= preference)
-    st.dataframe(top_products)
+    top_products = df_sorted[['product_name','brand_name']].head(n= preference)
+    columns_map = {'product_name':'Product','brand_name':'Brand'}
+    top_products.rename(columns = columns_map, inplace = True)
+    st.dataframe(top_products, hide_index=True)
 
 elif choice == 'TraitPicker':
-    st.title('Sephora Content-Based Recommendation System')
+    st.title('Trait Picker')
     col1, col2 = st.columns(2)
     # Dropdown for selecting brand
     with col1:
-        selected_brand = st.selectbox('Select Brand', df_product['brand_name'].unique())
+        selected_brand = st.selectbox('Brand', df_product['brand_name'].unique())
     # Filter products based on the selected brand
     filtered_products = df_product[df_product['brand_name'] == selected_brand]['product_name']
     # Dropdown for selecting product
     with col2:
-        selected_product = st.selectbox('Select Product', filtered_products)
+        selected_product = st.selectbox('Product', filtered_products)
     # Display the selected brand and product
     st.write(f'Selected Brand: {selected_brand}')
     st.write(f'Selected Product: {selected_product}')
@@ -167,26 +257,27 @@ elif choice == 'TraitPicker':
     product_table = tabulate(info_product, headers='keys', tablefmt='psql',showindex=False )
 
     # Button to get recommendations
-    if st.button('Get Recommendations'):
-        info_product = df_product[df_product['product_name'] == selected_product][['product_id','product_name', 'brand_name', 'primary_category','secondary_category' ,'tertiary_category']]
+    if st.button('Get Similar Recommendations'):
+        info_product = df_product[(df_product['product_name'] == selected_product) & (df_product['brand_name'] == selected_brand)][['product_id','product_name', 'brand_name', 'tertiary_category']]
         product_table = tabulate(info_product, headers='keys', tablefmt='psql',showindex=False )
-        st.write(info_product)
-        #recommendations = get_similar_recommendations(info_product['product_id'].values[0], df_product,similarity_matrix, top_n = preference)
-        recommendations = get_recommendations(info_product['product_id'].values[0], top_n=preference)
-        st.write('Similar products')
-        st.write(recommendations)
+        selected_product_id= info_product['product_id'].values[0]
+        selected_product_info = info_product[['product_name', 'brand_name', 'tertiary_category']]
+        columns_map = {'product_name':'Product','brand_name':'Brand', 'tertiary_category': 'Category'}
+        selected_product_info.rename(columns = columns_map, inplace = True)
+        st.dataframe(selected_product_info, hide_index=True)
+        similarity_matrix = compute_similarity_matrix()
+        recommendations = get_recommendations(selected_product_id, top_n=preference)
+        st.write('Similar Product Recommendations')
+        st.dataframe(recommendations, hide_index=True)
 
-
-elif choice == 'SquadSuggest':
-    st.header('Squad Recommender system')
+elif choice == 'SquadSuggester':
+    st.title('Squad Suggester')
     user_input = st.text_input('Enter the user id here:', '8447021668')
-    if st.button("Get recommendations for user"):
+    if st.button("Get squad suggestions"):
         rated_products = get_rated_products_by_user(user_input, df_review, df_product_info)
-        st.write("The products rated by userid ", user_input)
-        st.write(rated_products)
-        recs = get_user_recommendations(user_input, svd_best_model, df_review, df_product_info, n=preference)
-        st.write("The products recommended for  userid ", user_input)
-        st.write(recs)
-
-
-
+        st.write("The products rated by user ", user_input)
+        st.dataframe(rated_products, hide_index=True)
+        recs = get_user_recommendations(user_input, knn_best_model, df_review, df_product_info, n=preference)
+        st.write("The products recommended for user ", user_input)
+        st.dataframe(recs, hide_index=True)
+      
